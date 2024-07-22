ed2c8e7da45abf046819482b33a835d874ffdda5
nanjo0131 <149230539+nanjo0131@users.noreply.github.com>
2024-07-22 23:50:34 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,14 @@
+let num = Math.floor(Math.random() * 6);
+
+if (num = num * 3) {
+    console.log('3の倍数です');
+} 
+else if (num = num * 5) {
+    console.log('5の倍数です');
+}
+else if (num = 3 * 5) {
+    console.log('3と5の倍数です');
+}
+else {
+    console.log(num);
+}
