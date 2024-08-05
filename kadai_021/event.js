fea924f01d3379cbf59ff1123b875a07c96198c6
nanjo0131 <149230539+nanjo0131@users.noreply.github.com>
2024-08-05 22:33:27 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,13 @@
+const addText = document.getElementById('text');
+const addBtn = document.getElementById('btn');
+
+
+addBtn.addEventListener('click', () => {
+    setTimeout(() => {
+        addText.textContent = 'ボタンをクリックしました';
+    }, 2000);
+});
+
+
+   
+
