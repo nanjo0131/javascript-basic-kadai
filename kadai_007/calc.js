4b31cb4166b7cf5d71336b83146f20382808c108
nanjo0131 <149230539+nanjo0131@users.noreply.github.com>
2024-08-09 22:52:21 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,40 @@
+let num = 15;
+
+// 3の倍数
+if (num == 3 ) {
+    console.log('3の倍数です');
+} 
+
+if (num == 6 ) {
+    console.log('3の倍数です');
+} 
+
+if (num == 9 ) {
+    console.log('3の倍数です');
+} 
+
+// 5の倍数
+if(num == 5) {
+    console.log('5の倍数です');
+}
+
+if(num == 10) {
+    console.log('5の倍数です');
+}
+
+// 3と5の倍数
+if (num == 15 ) {
+    console.log('3と5の倍数です');
+}
+
+if (num == 30 ) {
+    console.log('3と5の倍数です');
+}
+
+// それ以外
+if (num == 4)  {
+    console.log(num);
+}
+if (num == 7)  {
+    console.log(num);
+}
